6a30da2c013919084a459fad8e31a4ec4d0d4e30
Marllon <marllonaraujo.silva@gmail.com>
2025-01-27 18:34:06 -0300

    Dados em componentes (data)

--- 2_projeto_vue/src/components/PrimeiroComponente.vue
+++ 2_projeto_vue/src/components/PrimeiroComponente.vue
@@ -1,9 +1,18 @@
 <template>
-    <h1>Olá Vue!</h1>
+    <div>
+        <h1>Olá Vue!</h1>
+        <p>Meu nome é {{nome }} e trabalho como {{ programador }}</p>
+    </div>
 </template>
 
 <script>
     export default {
-        name: 'PrimeiroComponente'
+        name: 'PrimeiroComponente',
+        data() {
+            return {
+                nome: "Marllon",
+                profissao: "Programador"
+            }
+        }
     }
 </script>
