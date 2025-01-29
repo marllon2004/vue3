7b559251062234535433e016bea15c1367e7123e
Marllon <marllonaraujo.silva@gmail.com>
2025-01-29 19:42:04 -0300

    Reutilizacao de componentes

--- 2_projeto_vue/src/components/PrimeiroComponente.vue
+++ 2_projeto_vue/src/components/PrimeiroComponente.vue
@@ -1,17 +1,23 @@
 <template>
     <div>
+        <Reutilizacao />
         <h1>Olá Vue!</h1>
         <p>Meu nome é {{nome }} e trabalho como {{ profissao }}.</p>
+        <Reutilizacao />
         <MultiplosEventos />
+        <Reutilizacao />
     </div>
 </template>
 
 <script>
 import MultiplosEventos from './MultiplosEventos.vue'
+import Reutilizacao from './Reutilizacao.vue'
+
     export default {
         name: 'PrimeiroComponente',
         components: {
-            MultiplosEventos
+            MultiplosEventos,
+            Reutilizacao
         },
         data() {
             return {
