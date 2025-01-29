dc97c9051da3719671657189cb705c48fcda8ba9
Marllon <marllonaraujo.silva@gmail.com>
2025-01-29 19:28:54 -0300

    Múltiplos eventos

---
+++ 2_projeto_vue/src/components/MultiplosEventos.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <button @click="primeiro('Primeiro evento ativado!', $event), segundo('Segundo evento ativado!', $event)">Ativar múltiplos eventos</button>
+        <p>{{ multiplos_eventos }}</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'MultiplosEventos',
+    data() {
+        return {
+            multiplos_eventos: ""
+        }
+    },
+    methods: {
+        primeiro(msg, e){
+            this.multiplos_eventos = msg
+        },
+        segundo(msg, e){
+            setTimeout(() => this.multiplos_eventos = msg, 500)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    div {
+        margin: 100px;
+    }
+</style>
