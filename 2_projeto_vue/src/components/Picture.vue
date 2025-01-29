6805e0947c5a392e460ba94862fe7e04934fa722
Marllon <marllonaraujo.silva@gmail.com>
2025-01-29 20:32:28 -0300

    Emit

--- 2_projeto_vue/src/components/Picture.vue
+++ 2_projeto_vue/src/components/Picture.vue
@@ -1,14 +1,29 @@
 <template>
-    <img :src="avatar" :alt="descricao">
+    <div>
+        <img :src="avatar" :alt="descricao">
+        <MudarImagem @mudar-imagem="trocarImagem" />
+    </div>
 </template>
 
 <script>
+import MudarImagem from './MudarImagem.vue'
+import Reutilizacao from './Reutilizacao.vue'
+
     export default {
         name: 'Picture',
+        components: { 
+            Reutilizacao,
+            MudarImagem
+        },
         data() {
             return {
                 avatar: "/img/avatar.png",
                 descricao: "Avatar"
+            }
+        },
+        methods: {
+            trocarImagem() {
+                this.avatar = "/img/avatar2.png"
             }
         }
     }
